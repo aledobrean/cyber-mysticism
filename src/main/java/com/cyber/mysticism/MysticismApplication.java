b907398101f843d007eed542449358ec30081590
Ana Alexandra Dobrean <adobrean@192-168-0-103.rdsnet.ro>
2023-12-16 17:41:09 +0200

    docs: Updated gitignore, added base spring boot config, added the tarot deck files

---
+++ src/main/java/com/cyber/mysticism/MysticismApplication.java
@@ -0,0 +1,13 @@
+package com.cyber.mysticism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysticismApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysticismApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cyber/mysticism/MysticismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyber.mysticism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysticismApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
